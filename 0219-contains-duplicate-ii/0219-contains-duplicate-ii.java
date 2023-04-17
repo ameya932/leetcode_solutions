7c2e83b045c12ec9c6c9854a57dade72af30c7a3
Ameya Dhanorkar <95168115+ameya932@users.noreply.github.com>
2023-04-17 21:54:42 +0530

    Time: 1223 ms (11.13%), Space: 53.9 MB (33.26%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0; i<nums.length; i++){
+            for(int j=Math.max(0,i-k); j<i; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
