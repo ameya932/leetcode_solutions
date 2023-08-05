88c30779c642a41934cba13252593b89cb21b526
Ameya Dhanorkar <95168115+ameya932@users.noreply.github.com>
2023-08-05 19:49:49 +0530

    Added solution - LeetHub

---
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] grid = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = sc.nextInt();
+                }
+            }
+            int[] source = new int[2];
+            for (int i = 0; i < 2; i++) {
+                int x = sc.nextInt();
+                source[i] = x;
+            }
+            int[] dest = new int[2];
+            for (int i = 0; i < 2; i++) {
+                int x = sc.nextInt();
+                dest[i] = x;
+            }
+            Solution ob = new Solution();
+            int ans = ob.shortestPath(grid, source, dest);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Tuple{
+    int first, second, third;
+    Tuple(int first, int second, int third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+class Solution {
+    int shortestPath(int[][] grid, int[] source, int[] destination) {
+        if(source[0]==destination[0] && source[1]==destination[1]){
+            return 0;
+        }
+        Queue<Tuple> q = new LinkedList<>();
+        int n = grid.length;
+        int m = grid[0].length;
+        int dist[][] = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dist[i][j] = (int)1e9;
+            }
+        }
+        dist[source[0]][source[1]] = 0;
+        q.add(new Tuple(0, source[0], source[1]));
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+        while(!q.isEmpty()){
+            Tuple it = q.peek();
+            q.remove();
+            int dis = it.first;
+            int r = it.second;
+            int c = it.third;
+            for(int i=0; i<4; i++){
+                int nrow = r+delrow[i];
+                int ncol = c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && dis+1<dist[nrow][ncol]){
+                    dist[nrow][ncol] = 1+dis;
+                    if(nrow==destination[0] && ncol==destination[1]){
+                        return dis+1;
+                    }
+                    q.add(new Tuple(dis+1, nrow, ncol));
+                }
+            }
+        }
+        return -1;
+    }
+}
