575ae4205337382bb879126dcfabc859cb3fa53d
Ameya Dhanorkar <95168115+ameya932@users.noreply.github.com>
2023-08-08 13:27:00 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.java
+++ Number of Provinces - GFG/number-of-provinces.java
@@ -31,36 +31,74 @@ class GFG {
 
 //User function Template for Java
 
-class Solution {
-    private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int vis[]) {
-        vis[node] = 1; 
-        for(Integer it : adjLs.get(node)) {
-            if(vis[it] == 0) {
-                dfs(it, adjLs, vis); 
-            }
+class DisjointSet{
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n){
+        for(int i=0; i<=n; i++){
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
         }
     }
-    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
-        ArrayList<ArrayList<Integer>> adjLs = new ArrayList<ArrayList<Integer>>(); 
-        for(int i=0; i<V; i++) {
-            adjLs.add(new ArrayList<Integer>()); 
+    public int findUPar(int node){
+        if(node == parent.get(node)){
+            return node;
         }
-        for(int i=0; i<V; i++) {
-            for(int j=0; j<V; j++) {
-                if(adj.get(i).get(j)==1 && i!=j) {
-                    adjLs.get(i).add(j); 
-                    adjLs.get(j).add(i); 
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+    public void unionByRank(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        else if(rank.get(ulp_u) < rank.get(ulp_v)){
+            parent.set(ulp_u, ulp_v);
+        }
+        else{
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU+1);
+        }
+    }
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v){
+            return; 
+        }
+        if(size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+class Solution {
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        DisjointSet ds = new DisjointSet(V);
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                if(adj.get(i).get(j) == 1){
+                    ds.unionBySize(i, j);
                 }
             }
         }
-        int vis[] = new int[V]; 
-        int count = 0; 
-        for(int i=0; i<V; i++) {
-            if(vis[i] == 0) {
-               count++;
-               dfs(i, adjLs, vis); 
+        int count = 0;
+        for(int i=0; i<V; i++){
+            if(ds.parent.get(i) == i){
+                count++;
             }
         }
-        return count; 
+        return count;
     }
 };
