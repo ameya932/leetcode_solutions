4cb831fa768c7cddaf3f68721798c50b1e310681
Ameya Dhanorkar <95168115+ameya932@users.noreply.github.com>
2023-10-11 11:23:12 +0530

    Time: 23 ms (5.49%), Space: 43.4 MB (44.58%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,7 @@
+import java.math.*;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        return String.valueOf((new BigInteger(num1)).add(new BigInteger(num2)));
+    }
+}
