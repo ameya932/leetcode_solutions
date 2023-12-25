4de735edfbc13720f2f127bdb1b766392cd69e26
Ameya Dhanorkar <95168115+ameya932@users.noreply.github.com>
2023-12-25 12:54:06 +0530

    Added solution - LeetHub

---
+++ Anagram of String - GFG/anagram-of-string.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//saksham raj seth
+import java.util.*;
+import java.util.stream.*;
+
+class Anagrams{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			String s=sc.next();
+			String s1=sc.next();
+			GfG g=new GfG();
+			System.out.println(g.remAnagrams(s,s1));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG
+{
+	public int remAnagrams(String s,String s1)
+    {
+        //add code here.
+        int arr[] = new int[26];
+        for(int i=0; i<s.length(); i++){
+            int num = s.charAt(i)-'a';
+            arr[num]++;
+        }
+        for(int i=0; i<s1.length(); i++){
+            int num = s1.charAt(i)-'a';
+            arr[num]--;
+        }
+        int count = 0;
+        for(int i=0; i<26; i++){
+            int num = Math.abs(arr[i]);
+            count += num;
+        }
+        return count;
+    }
+}
